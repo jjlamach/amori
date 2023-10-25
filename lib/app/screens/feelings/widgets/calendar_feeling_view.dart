e9d08f1fbf2cf6e3a04a9431bb73c9d07608b4bc
julama <lamajulio17@gmail.com>
2023-10-25 09:22:14 -0500

    Favorite Feeling
    - Favorite a feeling from Calendar view

--- lib/app/screens/feelings/widgets/calendar_feeling_view.dart
+++ lib/app/screens/feelings/widgets/calendar_feeling_view.dart
@@ -1,3 +1,4 @@
+import 'package:amori/app/screens/favorites/state/favorite_cubit.dart';
 import 'package:amori/app/screens/feelings/state/delete_feeling_cubit.dart';
 import 'package:amori/app/screens/signin/state/auth_bloc.dart';
 import 'package:amori/common/assets.dart';
@@ -95,54 +96,110 @@ class CalendarFeelingView extends StatelessWidget {
             ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Center(
-            child: GestureDetector(
-              onTap: () => showDialog(
-                context: context,
-                builder: (dialogContext) => AlertDialog(
-                  title: const Text(Strings.deleteFeeling),
-                  content: const Text(
-                    Strings.areYouSureYouWantToDelete,
-                  ),
-                  actions: [
-                    TextButton(
-                      onPressed: () async {
-                        AutoRouter.of(dialogContext).pop(); // Close the dialog
-                        final uid = context.read<AuthBloc>().getUser() ?? '';
-                        context
-                            .read<DeletionCubit>()
-                            .deleteFeeling(uid, feeling.dateTime);
-                      },
-                      child: const Text(
-                        Strings.yes,
-                        style: TextStyle(fontWeight: FontWeight.w700),
+        const SizedBox(height: 40),
+        SizedBox(
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () => showDialog(
+                  context: context,
+                  builder: (dialogContext) => AlertDialog(
+                    title: const Text(Strings.deleteFeeling),
+                    content: const Text(
+                      Strings.areYouSureYouWantToDelete,
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
+                          AutoRouter.of(dialogContext)
+                              .pop(); // Close the dialog
+                          final uid = context.read<AuthBloc>().getUser() ?? '';
+                          context
+                              .read<DeletionCubit>()
+                              .deleteFeeling(uid, feeling.dateTime);
+                        },
+                        child: const Text(
+                          Strings.yes,
+                          style: TextStyle(fontWeight: FontWeight.w700),
+                        ),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () => AutoRouter.of(dialogContext)
-                          .pop(), // Close the dialog
-                      child: const Text(Strings.no),
-                    ),
-                  ],
+                      TextButton(
+                        onPressed: () => AutoRouter.of(dialogContext)
+                            .pop(), // Close the dialog
+                        child: const Text(Strings.no),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              child: Container(
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color.fromRGBO(255, 226, 226, 1),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Image.asset(
-                    Assets.trashIcon,
-                    width: 31,
-                    height: 34,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromRGBO(255, 226, 226, 1),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Image.asset(
+                      Assets.trashIcon,
+                      width: 31,
+                      height: 34,
+                    ),
                   ),
                 ),
               ),
-            ),
+              const SizedBox(width: 20),
+              BlocBuilder<FavoriteCubit, bool>(
+                builder: (context, state) {
+                  final uid = context.read<AuthBloc>().uid ?? '';
+                  if (feeling.isFavorite) {
+                    return IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (dialogContext) => AlertDialog(
+                            title:
+                                const Text('Removing feeling from favorites'),
+                            content: const Text(
+                                'Are you sure you want to remove this Feeling from your Favorite list?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  context
+                                      .read<FavoriteCubit>()
+                                      .unfavoriteFeeling(uid, feeling.dateTime);
+                                  AutoRouter.of(dialogContext).pop();
+                                },
+                                child: const Text(
+                                  'Yes',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () =>
+                                    AutoRouter.of(dialogContext).pop(),
+                                child: const Text('No'),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      icon: const Icon(Icons.favorite,
+                          color: Colors.red, size: 40),
+                    );
+                  } else {
+                    return IconButton(
+                      onPressed: () {
+                        context
+                            .read<FavoriteCubit>()
+                            .favoriteFeeling(uid, feeling.dateTime);
+                      },
+                      icon: const Icon(Icons.favorite_border, size: 40),
+                    );
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ],
